da887148f5ef6d090ccc5d7007d1346b0d35ce64
Golden Eagle <joowoon.kang@biola.edu>
2020-08-11 22:53:17 -0700

    attempting to add marketing page

--- src/components/pages/ExampleDataViz/RenderDataViz.js
+++ src/components/pages/ExampleDataViz/RenderDataViz.js
@@ -1,45 +1,46 @@
 /*eslint no-unused-vars: 0 */
 import React, { useState, useEffect } from 'react';
-import Plot from 'react-plotly.js';
+// import Plot from 'react-plotly.js';
 import { getDSData } from '../../../api';
-
-const initialState = {
-  data: [],
-  layout: {},
-  frames: [],
-  config: {
-    displaylogo: false,
-    displayModeBar: false,
-  },
-};
+import ReactMapGL from 'react-map-gl';
 
 function DataViz(props) {
-  const [data, setData] = useState(initialState);
-  const [figure, setFigure] = useState(null);
+  const [data, setData] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10,
+  });
+  // const [figure, setFigure] = useState(null);
 
-  useEffect(() => {
-    function fetchDSData() {
-      getDSData(props.url, props.authState)
-        .then(res => {
-          setData(res);
-        })
-        .catch(err => {
-          setData({ data: null, err });
-        });
-    }
-    fetchDSData();
-  }, [props.url, props.authState]);
-
+  // useEffect(() => {
+  //   function fetchDSData() {
+  //     getDSData(props.url, props.authState)
+  //       .then(res => {
+  //         setData(res);
+  //       })
+  //       .catch(err => {
+  //         setData({});
+  //       });
+  //   }
+  //   fetchDSData();
+  // }, [props.url, props.authState]);
+  console.log(process.env.REACT_APP_MAPBOX_TOKEN);
   return (
-    <Plot
-      className="DataViz"
-      data={data.data}
-      layout={data.layout}
-      frames={data.frames}
-      config={data.config}
-      onInitialized={figure => setFigure(figure)}
-      onUpdate={figure => setFigure(figure)}
-    />
+    <ReactMapGL
+      {...data}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKKEN}
+      // className="DataViz"
+      // data={data.data}
+      // layout={data.layout}
+      // frames={data.frames}
+      // config={data.config}
+      // onInitialized={figure => setFigure(figure)}
+      // onUpdate={figure => setFigure(figure)}
+    >
+      Markers here
+    </ReactMapGL>
   );
 }
 
