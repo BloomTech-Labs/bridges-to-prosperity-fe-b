48f506c6a9f2470102b6f502f1cbcf0a8d4c3d95
Devin_Moore <d.j.moore209@gmail.com>
2020-08-19 17:58:45 -0400

    styled the nav bar to mirror guidelines, adjusted map page layout

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,53 +1,17 @@
-import React, { useState } from 'react';
-import { Menu, Input } from 'antd';
-import { MailOutlined, SettingOutlined } from '@ant-design/icons';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const { SubMenu } = Menu;
-const { Search } = Input;
-
-const initalState = {
-  current: 'mail',
-};
-
 const Navigation = props => {
-  const [navData, setNavData] = useState(initalState);
-
-  const handleClick = e => {
-    // console.log('click ', e);
-    setNavData({ current: e.key });
-  };
-
   return (
-    <>
-      <Menu
-        onClick={e => handleClick(e)}
-        selectedKeys={navData.current}
-        mode="horizontal"
-      >
-        <Search
-          placeholder="input search text"
-          onSearch={value => console.log(value)}
-          style={{ width: 200 }}
-        />
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <SubMenu icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <Link to="/login">Login</Link>
-        </Menu.Item>
-      </Menu>
-    </>
+    <div className="nav">
+      <img
+        src={require('../../styles/imgs/B2P_PrimaryLockup_Green-Navy.svg')}
+        alt="Bridges of Prosperity Logo"
+      />
+      <Link to="/login" className="loginButton">
+        Login
+      </Link>
+    </div>
   );
 };
 
