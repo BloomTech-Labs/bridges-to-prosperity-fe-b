5c5400ee667a775852f5f6b7d195ee4e93e80f27
Devin_Moore <d.j.moore209@gmail.com>
2020-08-24 16:55:00 -0400

    rendering details in component

--- src/components/pages/HomePage/DetailsSection.js
+++ src/components/pages/HomePage/DetailsSection.js
@@ -12,6 +12,24 @@ const Details = ({ props }) => {
           {detailsData.Province ? <p>{detailsData.Province}</p> : null}
           <p>District: {detailsData.district}</p>
           <p>Project Stage: {detailsData.project_stage}</p>
+          {detailsData.sub_stage ? (
+            <p>Project Sub-Stage: {detailsData.sub_stage}</p>
+          ) : null}
+          {detailsData.type ? <p>Type: {detailsData.type}</p> : null}
+          {detailsData.individuals_served ? (
+            <p>individuals Served: {detailsData.individuals_served}</p>
+          ) : null}
+          {detailsData.communities_served ? (
+            <div>
+              <p>Communities Served: </p>
+              <ul>
+                {detailsData.communities_served.map(community => (
+                  <li key={community.id}>{community.name}</li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
+          {detailsData.span !== 'NaN' ? <p>Span: {detailsData.span}</p> : null}
         </div>
       ) : null}
     </div>
