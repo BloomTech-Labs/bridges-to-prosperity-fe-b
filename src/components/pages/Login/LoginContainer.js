5bd718686ed4eb2d83784c4906949baa51197e81
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-15 21:49:46 -0500

    Added Bridges to Prosperity logo and changed login text.

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -17,11 +17,12 @@ const LoginContainer = () => {
       },
       features: { registration: false },
       // turning this feature on allows your widget to use Okta for user registration
-      logo: 'path-to-your-logo',
+      logo:
+        'https://s3.amazonaws.com/bridgestoprosperity-teamb/B2P_Symbol_Green.png',
       // add your custom logo to your signing/register widget here.
       i18n: {
         en: {
-          'primaryauth.title': 'Welcome to Labs Basic SPA Please sign in',
+          'primaryauth.title': 'Sign In',
           // change title for your app
         },
       },
