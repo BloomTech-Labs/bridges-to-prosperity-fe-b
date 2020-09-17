6c8bdd994899b27297c6a287f3a636a4dbf35537
Devin_Moore <d.j.moore209@gmail.com>
2020-09-17 13:38:06 -0400

    added styling to table details component

--- src/components/pages/DetailsPage/RenderDetails.js
+++ src/components/pages/DetailsPage/RenderDetails.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { BridgesContext } from '../../../state/bridgesContext';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import Navigation from '../../common/Navigation';
 import Footer from '../../common/Footer';
 
@@ -13,27 +13,47 @@ function DetailsCard() {
   }
 
   return (
-    <div>
+    <>
       <Navigation />
-      {detailsData && (
-        <div>
-          <div>{detailsData.b2p_bridge_id}</div>
-          <div>{detailsData.id}</div>
-          <div>{detailsData.project_code}</div>
-          <div>{detailsData.country}</div>
-          <div>{detailsData.district}</div>
-          <div>{detailsData.province}</div>
-          <div>{detailsData.sector}</div>
-          <div>{detailsData.lat}</div>
-          <div>{detailsData.long}</div>
-          <div>{detailsData.project_stage}</div>
-          <div>{detailsData.bridge_type}</div>
-          <div>{detailsData.span}</div>
-          <div>Communities Served: </div>
-        </div>
-      )}
+      <div className="mainContainer">
+        {detailsData && (
+          <div className="cardContainer">
+            <div className="top">
+              <p>Id: {detailsData.id}</p>
+              <p>Bridge Opportunity Id: {detailsData.bridge_opportunity_id}</p>
+              <p>Project Code: {detailsData.project_code}</p>
+            </div>
+            <div className="title">
+              <p>Project Stage: {detailsData.project_stage}</p>
+              <p>Project Sub Stage: {detailsData.project_sub_stage}</p>
+              <p>Bridge Site Name: {detailsData.bridge_site_name}</p>
+              <p>Bridge Type: {detailsData.bridge_type}</p>
+            </div>
+            <div className="info">
+              <p>Country: {detailsData.country}</p>
+              <p>District: {detailsData.district}</p>
+              <p>Province: {detailsData.province}</p>
+              <p>Sector: {detailsData.sector}</p>
+              <p>Latitude: {detailsData.lat}</p>
+              <p>Longitude: {detailsData.long}</p>
+              <p>Span: {detailsData.span}</p>
+              <p>
+                Inpiduals Directly Served:{' '}
+                {detailsData.inpiduals_directly_served}
+              </p>
+            </div>
+            <div className="communitiesServed">
+              <p>Communities Served: </p>
+            </div>
+            <div className="links">
+              <Link to="/form">Edit</Link>
+              <Link to="/table">Back</Link>
+            </div>
+          </div>
+        )}
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
