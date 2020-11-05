85a6c00410fb78e777c90478203364e68b64f449
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-11-04 21:23:21 -0400

    Update Navigation.js
    Add Grid and updated button etc

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,53 +1,98 @@
 import React from 'react';
+import { Grid, Typography, Button } from '@material-ui/core';
 import { Link, useHistory } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    height: '100px',
+    padding: '0 20px',
+  },
+  logo: {
+    width: '50px',
+  },
+});
 
 const Navigation = props => {
+  const classes = useStyles();
+
   const { authService } = useOktaAuth();
   const { push } = useHistory();
 
   return (
-    <div className="nav">
-      <div
+    <Grid
+      container
+      spacing={2}
+      xs={12}
+      justify="space-between"
+      className={classes.root}
+      alignItems="center"
+    >
+      <Grid
+        item
+        container
+        alignItems="center"
+        spacing={1}
+        xs={6}
         onClick={() => {
           push('/');
         }}
-        className="logo"
       >
-        <img
-          src={require('../../styles/imgs/B2P_Symbol_Green.png')}
-          alt="Bridges of Prosperity Logo"
-        />
-        <h1>Bridges to Prosperity</h1>
-      </div>
-      <div className="links">
-        <Link to="/" className="navLinks">
-          Home
-        </Link>
-        {!localStorage.getItem('okta-pkce-storage') ? (
-          <Link to="/login" className="loginButton">
-            Login
+        <Grid item>
+          <img
+            src={require('../../styles/imgs/B2P_Symbol_Green.png')}
+            alt="Bridges of Prosperity Logo"
+            className={classes.logo}
+          />
+        </Grid>
+        <Grid item>
+          <Typography variant="h4">Bridges to Prosperity</Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        xs={6}
+        justify="flex-end"
+        spacing={2}
+        alignItems="center"
+      >
+        <Grid item>
+          <Link to="/" className="navLinks">
+            <Button variant="contained">Home</Button>
           </Link>
-        ) : (
-          <>
-            <Link to="/table" className="navLinks">
-              Table
+        </Grid>
+        <Grid item>
+          {!localStorage.getItem('okta-pkce-storage') ? (
+            <Link to="/login" className="loginButton" color="primary">
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
             </Link>
-            <Link
-              to="/"
-              onClick={() => {
-                authService.logout();
-                localStorage.removeItem('okta-cache-storage');
-                localStorage.removeItem('okta-pkce-storage');
-              }}
-              className="loginButton"
-            >
-              Logout
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+          ) : (
+            <>
+              <Link to="/table" className="navLinks">
+                Table
+              </Link>
+              <Link
+                to="/"
+                onClick={() => {
+                  authService.logout();
+                  localStorage.removeItem('okta-cache-storage');
+                  localStorage.removeItem('okta-pkce-storage');
+                }}
+                className="loginButton"
+              >
+                <Button variant="contained" color="primary">
+                  Logout
+                </Button>
+              </Link>
+            </>
+          )}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
