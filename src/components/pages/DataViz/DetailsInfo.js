adef817ef0cdfc48a8d45830b3a8badb61399a3d
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-11-05 16:50:05 -0500

    hunted down stying for modal and removed unecessary scss file

--- src/components/pages/DataViz/DetailsInfo.js
+++ src/components/pages/DataViz/DetailsInfo.js
@@ -52,44 +52,46 @@ const DetailsInfo = props => {
           <h2>
             <strong>{detailsData.bridge_site_name}</strong>
           </h2>
+
+          <div>
+            <p>
+              {detailsData.province} - {detailsData.district}
+            </p>
+          </div>
+
+          <div></div>
+
           <div className="bridge-image">
             {detailsData.bridge_image ? (
               <div className="bridge-image">
                 <img alt="bridge_image" src={`${detailsData.bridge_image}`} />
               </div>
             ) : (
-              <div className="bridge-image">
-                {/* <img
-                  alt="bridge_image_needed"
-                  src={require('../../../styles/imgs/bridgeIconGreenBig.png')}
-                /> */}
-                No Bridge Image
-              </div>
+              <div className="bridge-image">No Bridge Image</div>
             )}
           </div>
 
-          {/* <p>Bridge Site Name: {detailsData.bridge_site_name}</p> */}
           <div className="bridge_info">
             <p className="info">
               <span className="infoLabel">Project Stage:</span>
               {detailsData.project_stage}
             </p>
-            <p className="info">
+            {/* <p className="info">
               <span className="infoLabel">Province:</span>
               {detailsData.province}
             </p>
             <p className="info">
               <span className="infoLabel">District:</span>
               {detailsData.district}
-            </p>
+            </p> */}
             <p className="info">
               <span className="infoLabel">Bridge Type:</span>{' '}
               {detailsData.bridge_type}
             </p>
-            {/* <p className="info">
+            <p className="info">
               <span className="infoLabel">Project Sub Stage:</span>{' '}
               {detailsData.sub_stage}
-            </p> */}
+            </p>
             {detailsData.project_stage === 'Prospecting' ? (
               <p className="info">
                 <span className="infoLabel"> GDP per Capita:</span>
@@ -105,17 +107,6 @@ const DetailsInfo = props => {
                 </button>
               </p>
             ) : null}
-            {/* <p className="info">
-              <span className="infoLabel"> GDP per Capita:</span>
-              <a
-                className="dataIcon"
-                role="img"
-                style={{ fontSize: '1.2rem' }}
-                onClick={event => openData(event)}
-              >
-                📊
-              </a>
-            </p> */}
           </div>
         </div>
       </div>
