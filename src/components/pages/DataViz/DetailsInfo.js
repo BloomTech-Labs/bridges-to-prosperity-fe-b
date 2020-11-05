35b54449f5fc9b65c65a58101e52e94e70772ae9
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-11-05 17:31:53 -0500

    reorganized jsx for detailsInfo to remove redundancy

--- src/components/pages/DataViz/DetailsInfo.js
+++ src/components/pages/DataViz/DetailsInfo.js
@@ -49,65 +49,45 @@ const DetailsInfo = props => {
         </div>
 
         <div className="detailsInfo">
-          <h2>
-            <strong>{detailsData.bridge_site_name}</strong>
-          </h2>
+          <h2>{detailsData.bridge_site_name}</h2>
 
-          <div>
-            <p>
-              {detailsData.province} - {detailsData.district}
-            </p>
-          </div>
+          <p>
+            {detailsData.province} - {detailsData.district}
+          </p>
+          <p>{detailsData.project_stage}</p>
 
-          <div></div>
-
+          {/* Conditional Render of Image if availible */}
           <div className="bridge-image">
             {detailsData.bridge_image ? (
               <div className="bridge-image">
                 <img alt="bridge_image" src={`${detailsData.bridge_image}`} />
               </div>
-            ) : (
-              <div className="bridge-image">No Bridge Image</div>
-            )}
-          </div>
-
-          <div className="bridge_info">
-            <p className="info">
-              <span className="infoLabel">Project Stage:</span>
-              {detailsData.project_stage}
-            </p>
-            {/* <p className="info">
-              <span className="infoLabel">Province:</span>
-              {detailsData.province}
-            </p>
-            <p className="info">
-              <span className="infoLabel">District:</span>
-              {detailsData.district}
-            </p> */}
-            <p className="info">
-              <span className="infoLabel">Bridge Type:</span>{' '}
-              {detailsData.bridge_type}
-            </p>
-            <p className="info">
-              <span className="infoLabel">Project Sub Stage:</span>{' '}
-              {detailsData.sub_stage}
-            </p>
-            {detailsData.project_stage === 'Prospecting' ? (
-              <p className="info">
-                <span className="infoLabel"> GDP per Capita:</span>
-                <button
-                  className="dataIcon"
-                  role="img"
-                  style={{ fontSize: '1.2rem' }}
-                  onClick={event => openData(event)}
-                >
-                  <span role="img" aria-label="graph">
-                    📊
-                  </span>
-                </button>
-              </p>
             ) : null}
           </div>
+
+          <p>{detailsData.bridge_type}</p>
+
+          {/* Conditional render of substage if known */}
+          {detailsData.sub_stage !== '?' ? (
+            <p>{detailsData.sub_stage}</p>
+          ) : null}
+
+          {/* Conditional Render of GDP Button */}
+          {detailsData.project_stage === 'Prospecting' ? (
+            <p className="info">
+              <span className="infoLabel"> GDP per Capita:</span>
+              <button
+                className="dataIcon"
+                role="img"
+                style={{ fontSize: '1.2rem' }}
+                onClick={event => openData(event)}
+              >
+                <span role="img" aria-label="graph">
+                  📊
+                </span>
+              </button>
+            </p>
+          ) : null}
         </div>
       </div>
     </Draggable>
